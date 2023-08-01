d67d59857a8da47628acd9e5bf7928fbe966ea7f
wangli <wang.li@zxrail.com.cn>
2023-08-01 21:08:26 +0800

    危险源排查记录

---
+++ src/main/java/com/wzmtr/eam/controller/secure/SecureDangerSourceController.java
@@ -0,0 +1,15 @@
+package com.wzmtr.eam.controller.secure;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Author: Li.Wang
+ * Date: 2023/8/1 21:04
+ */
+@RestController
+@RequestMapping("/secure/danger")
+@Api(tags = "安全管理-危险源排查记录")
+public class SecureDangerSourceController {
+}
