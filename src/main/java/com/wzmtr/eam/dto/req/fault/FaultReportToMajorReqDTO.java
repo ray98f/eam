2acc0da082aceeb0c79ae8eba67d520e873dde8b
Li.Wang <wang.li@zxrail.com.cn>
2023-08-17 15:00:47 +0800

    入参修改

--- src/main/java/com/wzmtr/eam/dto/req/fault/FaultReportToMajorReqDTO.java
+++ src/main/java/com/wzmtr/eam/dto/req/fault/FaultReportToMajorReqDTO.java
@@ -16,50 +16,42 @@ import lombok.Data;
  */
 @Data
 @ApiModel
-public class FaultReportReqDTO {
+public class FaultReportToMajorReqDTO {
     @ApiModelProperty(value = "故障编号")
     private String faultNo;
-    @ApiModelProperty(value = "对象编码")
-    private String objectCode;
-    @ApiModelProperty(value = "对象名称")
-    private String objectName;
-    @ApiModelProperty(value = "车底号/车厢号")
-    private String trainTrunk;
-    @ApiModelProperty(value = "位置1")
-    private String positionCode;
-    @ApiModelProperty(value = "对象编码")
-    private String objectCodeTextField;
-    @ApiModelProperty(value = "线别姓名")
-    private String lineName;
-    @ApiModelProperty(value = "线别")
-    private String lineCode;
-    @ApiModelProperty(value = "位置2")
-    private String position2Name;
-    @ApiModelProperty(value = "位置2编码")
-    private String position2Code;
     @ApiModelProperty(value = "专业")
     private String majorName;
     @ApiModelProperty(value = "系统")
     private String systemName;
     @ApiModelProperty(value = "设备分类")
     private String equipTypeName;
+    @ApiModelProperty(value = "设备分类代码")
+    private String equipTypeCode;
     @ApiModelProperty(value = "专业code")
     private String majorCode;
     @ApiModelProperty(value = "系统code")
     private String systemCode;
-    @ApiModelProperty(value = "设备分类代码")
-    private String equipTypeCode;
+    @ApiModelProperty(value = "线别code")
+    private String lineCode;
+    @ApiModelProperty(value = "位置1")
+    private String positionCode;
+    @ApiModelProperty(value = "位置2")
+    private String position2Name;
+    @ApiModelProperty(value = "位置2编码")
+    private String position2Code;
+    @ApiModelProperty(value = "线别姓名")
+    private String lineName;
+    @ApiModelProperty(value = "故障分类（10-运营故障；20-自检故障；30-新线调试；40-正线故障；50-出库故障）")
+    private String faultType;
     @ApiModelProperty(value = "故障模块")
     private String faultModule;
     @ApiModelProperty(value = "故障模块Id")
     private String faultModuleId;
-    @ApiModelProperty(value = "故障分类（10-运营故障；20-自检故障；30-新线调试；40-正线故障；50-出库故障）")
-    private String faultType;
     @ApiModelProperty(value = "来源编号")
     private String sourceCode;
     @ApiModelProperty(value = "发现时间")
     private String discoveryTime;
-    @ApiModelProperty(value = "发现人")
+    @ApiModelProperty(value = "发现人姓名")
     private String discovererName;
     @ApiModelProperty(value = "发现人工号")
     private String discovererId;
@@ -67,8 +59,6 @@ public class FaultReportReqDTO {
     private String discovererPhone;
     @ApiModelProperty(value = "知会OCC调度")
     private String maintenance;
-    @ApiModelProperty(value = "故障现象")
-    private String faultDisplayDetail;
     @ApiModelProperty(value = "提报时间")
     private String fillinTime;
     @ApiModelProperty(value = "提报人工号")
@@ -79,8 +69,6 @@ public class FaultReportReqDTO {
     private String fillinDeptCode;
     @ApiModelProperty(value = "提报部门")
     private String fillinDeptName;
-    @ApiModelProperty(value = "故障状态")
-    private String orderStatus;
     @ApiModelProperty(value = "牵头部门")
     private String respDeptCode;
     @ApiModelProperty(value = "配合部门")
@@ -93,7 +81,7 @@ public class FaultReportReqDTO {
     private String faultDetail;
 
 
-    public FaultOrderBO toFaultOrderBO(FaultReportReqDTO req) {
+    public FaultOrderBO toFaultOrderBO(FaultReportToMajorReqDTO req) {
         FaultOrderBO convert = __BeanUtil.convert(req, FaultOrderBO.class);
         if (StringUtils.isNotEmpty(req.getRepairDeptCode())) {
             convert.setWorkClass(req.getRepairDeptCode());
@@ -106,7 +94,7 @@ public class FaultReportReqDTO {
         convert.setRecCreateTime(DateUtil.current(DateUtil.YYYY_MM_DD_HH_MM_SS));
         return convert;
     }
-    public FaultInfoBO toFaultInfoBO(FaultReportReqDTO req) {
+    public FaultInfoBO toFaultInfoBO(FaultReportToMajorReqDTO req) {
         FaultInfoBO convert = __BeanUtil.convert(req, FaultInfoBO.class);
         convert.setRecId(TokenUtil.getUuId());
         convert.setFillinTime(DateUtil.current(DateUtil.YYYY_MM_DD_HH_MM_SS));
