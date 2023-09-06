997a19fd02f580fc30b889d7a309cdf7c5f4fd98
Ray <fengruipeng>
2023-09-06 14:23:04 +0800

    设备台账二维码生成修改

--- src/main/java/com/wzmtr/eam/service/equipment/EquipmentService.java
+++ src/main/java/com/wzmtr/eam/service/equipment/EquipmentService.java
@@ -1,9 +1,11 @@
 package com.wzmtr.eam.service.equipment;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.wzmtr.eam.dto.res.EquipmentQrResDTO;
 import com.wzmtr.eam.dto.res.EquipmentResDTO;
 import com.wzmtr.eam.dto.res.EquipmentTreeResDTO;
 import com.wzmtr.eam.dto.res.RegionResDTO;
+import com.wzmtr.eam.entity.BaseIdsEntity;
 import com.wzmtr.eam.entity.PageReqDTO;
 import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,5 +31,5 @@ public interface EquipmentService {
     void exportEquipment(String equipCode, String equipName, String useLineNo, String useSegNo, String position1Code, String majorCode,
                          String systemCode, String equipTypeCode, String brand, String startTime, String endTime, String manufacture, HttpServletResponse response);
 
-    String generateQr(String id) throws ParseException;
+    List<EquipmentQrResDTO> generateQr(BaseIdsEntity baseIdsEntity) throws ParseException;
 }
