883dc88175d06d24873a5dac0cf5d0e5986f2e81
Ray <fengruipeng>
2023-09-08 09:46:15 +0800

    fix

--- src/main/java/com/wzmtr/eam/controller/fault/FaultQueryController.java
+++ src/main/java/com/wzmtr/eam/controller/fault/FaultQueryController.java
@@ -2,11 +2,7 @@ package com.wzmtr.eam.controller.fault;
 
 import com.wzmtr.eam.dto.req.fault.FaultDetailReqDTO;
 import com.wzmtr.eam.dto.req.fault.FaultQueryReqDTO;
-<<<<<<< HEAD
-=======
 import com.wzmtr.eam.dto.req.fault.FaultSubmitReqDTO;
-import com.wzmtr.eam.dto.res.fault.AnalyzeResDTO;
->>>>>>> 6dd1583ae04d1bc01101d96f3ba7b6c0f071ccc9
 import com.wzmtr.eam.dto.res.fault.ConstructionResDTO;
 import com.wzmtr.eam.dto.res.fault.FaultDetailResDTO;
 import com.wzmtr.eam.entity.SidEntity;
