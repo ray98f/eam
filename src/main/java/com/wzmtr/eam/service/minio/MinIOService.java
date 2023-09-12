0276c5f06518ec4078d5b9ff09387fb2402fab1f
Li.Wang <wang.li@zxrail.com.cn>
2023-09-12 11:02:04 +0800

    Revert "报错修复"
    This reverts commit bed9b3b5795ba6e59d2986e4e58cc38139dc7373.

--- src/main/java/com/wzmtr/eam/service/minio/MinIOService.java
+++ src/main/java/com/wzmtr/eam/service/minio/MinIOService.java
@@ -1,4 +1,4 @@
-package com.wzmtr.eam.service.minio;
+package com.wzmtr.eam.service.MinIO;
 
 import com.wzmtr.eam.entity.File;
 import org.springframework.web.multipart.MultipartFile;
@@ -7,6 +7,6 @@ import org.springframework.web.multipart.MultipartFile;
  * Author: Li.Wang
  * Date: 2023/8/11 15:36
  */
-public interface MinioService {
+public interface MinIOService {
     File upload(MultipartFile file, String bucketCode);
 }
