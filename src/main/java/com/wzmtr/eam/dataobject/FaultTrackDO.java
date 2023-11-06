4efbab7ae29fefe9119ec3515443fefc47c867ad
Li.Wang <wang.li@zxrail.com.cn>
2023-11-06 17:42:07 +0800

    fix

--- src/main/java/com/wzmtr/eam/dataobject/FaultTrackDO.java
+++ src/main/java/com/wzmtr/eam/dataobject/FaultTrackDO.java
@@ -1,53 +1,55 @@
 package com.wzmtr.eam.dataobject;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 /**
  * Author: Li.Wang
- * Date: 2023/9/5 15:49
+ * String: 2023/9/5 15:49
  */
 @Data
 @TableName("T_FAULT_TRACK")
 public class FaultTrackDO {
     @TableId(value = "REC_ID")
     private String recId;
-    /*  20 */   private String companyCode;
-    /*  20 */   private String workClass;
-    /*  21 */   private String companyName;
-    /*  22 */   private String faultTrackNo;
-    /*  23 */   private String faultAnalysisNo;
-    /*  24 */   private String faultNo;
-    /*  25 */   private String faultWorkNo;
-    /*  26 */   private String trackReason;
-    /*  27 */   private String trackUserId;
-    /*  28 */   private String trackTime;
-    /*  29 */   private Integer trackPeriod;
-    /*  30 */   private Integer trackCycle;
-    /*  31 */   private String trackStartDate;
-    /*  32 */   private String trackEndDate;
-    /*  33 */   private String trackReporterId;
-    /*  34 */   private String trackReportTime;
-    /*  35 */   private String trackResult;
-    /*  36 */   private String trackCloserId;
-    /*  37 */   private String trackCloseTime;
-    /*  38 */   private String workFlowInstId;
-    /*  39 */   private String workFlowInstStatus;
-    /*  40 */   private String docId;
-    /*  41 */   private String remark;
-    /*  42 */   private String recCreator;
-    /*  43 */   private String recCreateTime;
-    /*  44 */   private String recRevisor;
-    /*  45 */   private String recReviseTime;
-    /*  46 */   private String recDeletor;
-    /*  47 */   private String recDeleteTime;
-    /*  48 */   private String deleteFlag;
-    /*  49 */   private String archiveFlag;
-    /*  50 */   private String recStatus;
-    /*  51 */   private String ext1;
-    /*  52 */   private String ext2;
-    /*  53 */   private String ext3;
-    /*  54 */   private String ext4;
-    /*  55 */   private String ext5;
+    private String companyCode;
+    private String companyName;
+    private String faultTrackNo;
+    private String faultNo;
+    private String faultWorkNo;
+    private String faultAnalysisNo;
+    private String trackReason;
+    private String trackUserId;
+    private String trackTime;
+    private String trackStartDate;
+    private String trackEndDate;
+    private String trackPeriod;
+    private Integer trackCycle;
+    private String trackReporterId;
+    private String trackReportTime;
+    private String trackResult;
+    private String trackCloserId;
+    private String trackCloseTime;
+    private String workFlowInstId;
+    private String workFlowInstStatus;
+    private String docId;
+    private String remark;
+    private String recCreator;
+    private String recCreateTime;
+    private String recRevisor;
+    private String recReviseTime;
+    private String recDeletor;
+    private String recDeleteTime;
+    private String deleteFlag;
+    private String archiveFlag;
+    private String recStatus;
+    private String ext1;
+    private String ext2;
+    private String ext3;
+    private String ext4;
+    private String ext5;
+    @TableField(exist = false)
+    /*  20 */ private String workClass;
 }
