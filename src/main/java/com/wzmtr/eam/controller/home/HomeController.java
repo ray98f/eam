44f03f17e93420aad791ec89e7d38af3058d47e8
Ray <fengruipeng>
2024-01-31 19:25:28 +0800

    feat:新增首页工作台接口

--- src/main/java/com/wzmtr/eam/controller/home/HomeController.java
+++ src/main/java/com/wzmtr/eam/controller/home/HomeController.java
@@ -2,18 +2,24 @@ package com.wzmtr.eam.controller.home;
 
 import com.wzmtr.eam.dto.res.home.EChartResDTO;
 import com.wzmtr.eam.dto.res.home.HomeCountResDTO;
+import com.wzmtr.eam.entity.PageReqDTO;
+import com.wzmtr.eam.entity.StatusWorkFlowLog;
 import com.wzmtr.eam.entity.response.DataResponse;
+import com.wzmtr.eam.entity.response.PageResponse;
 import com.wzmtr.eam.service.home.HomeService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.poi.ss.formula.functions.T;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 /**
- * Author: Li.Wang
- * Date: 2023/9/12 11:22
+ * 首页管理
+ * @author  Ray
+ * @version 1.0
+ * @date 2023/09/12
  */
 @RestController
 @RequestMapping("/home")
@@ -26,6 +32,36 @@ public class HomeController {
     @ApiOperation(value = "数量统计(待办待阅等等。。。)")
     public DataResponse<HomeCountResDTO> count() {
         return DataResponse.of(homeService.count());
+    }
+
+    /**
+     * 获取首页工作台列表
+     * @param type 类型 1：待办 2：已办
+     * @param pageReqDTO 分页信息
+     * @return 工作列表
+     */
+    @GetMapping("/todoList")
+    public PageResponse<StatusWorkFlowLog> todoList(@RequestParam String type, @Valid PageReqDTO pageReqDTO) {
+        return PageResponse.of(homeService.todoList(type, pageReqDTO));
+    }
+
+    /**
+     * 返回工作台工作数量
+     * @return 工作数量
+     */
+    @GetMapping("/todoCount")
+    public DataResponse<HomeCountResDTO> todoCount() {
+        return DataResponse.of(homeService.todoCount());
+    }
+
+    /**
+     * 催办
+     * @param todoId 代办id
+     */
+    @GetMapping("/urging")
+    public DataResponse<T> urgingTodo(@RequestParam String todoId) {
+        homeService.urgingTodo(todoId);
+        return DataResponse.success();
     }
 
     @ApiOperation(value = "图表查询")
--- src/main/java/com/wzmtr/eam/dto/req/common/EipMsgPushReq.java
+++ src/main/java/com/wzmtr/eam/dto/req/common/EipMsgPushReq.java
@@ -26,31 +26,31 @@ public class EipMsgPushReq {
     public void add() {
         this.operId = "1";
         this.todoStatus = "1";
-        this.kindType = "1";
+        this.kindType = "3";
     }
 
     public void update() {
         this.operId = "2";
         this.todoStatus = "2";
-        this.kindType = "1";
+        this.kindType = "3";
     }
 
     public void readed() {
         this.operId = "1";
         this.todoStatus = "2";
-        this.kindType = "1";
+        this.kindType = "3";
     }
 
     public void delete() {
         this.operId = "3";
         this.todoStatus = "1";
-        this.kindType = "1";
+        this.kindType = "3";
     }
 
     public void send() {
         this.operId = "1";
         this.todoStatus = "7";
-        this.kindType = "1";
+        this.kindType = "3";
     }
 
 }
