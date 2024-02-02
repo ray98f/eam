417c36383617844aab9dd9e3f93fdc9174fd323a
Li.Wang <wang.li@zxrail.com.cn>
2024-02-02 14:55:53 +0800

    feat:开放接口支持位置名称查询

--- src/main/java/com/wzmtr/eam/mapper/fault/FaultReportMapper.java
+++ src/main/java/com/wzmtr/eam/mapper/fault/FaultReportMapper.java
@@ -4,9 +4,11 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.wzmtr.eam.dataobject.FaultInfoDO;
 import com.wzmtr.eam.dataobject.FaultOrderDO;
 import com.wzmtr.eam.dto.req.fault.FaultCancelReqDTO;
+import com.wzmtr.eam.dto.req.fault.FaultReportPageReqDTO;
 import com.wzmtr.eam.dto.res.fault.FaultOrderResDTO;
 import com.wzmtr.eam.dto.res.fault.FaultReportResDTO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -24,6 +26,8 @@ public interface FaultReportMapper {
     Page<FaultReportResDTO> list(Page<Object> of, String faultNo, String objectCode, String objectName,
                                  String faultModuleId, String majorCode, String systemCode, String equipTypeCode,
                                  String fillinTimeStart, String fillinTimeEnd, String positionCode, String orderStatus,String faultWorkNo,String lineCode);
+
+    Page<FaultReportResDTO> openApiList(Page<Object> of, @Param("reqDTO") FaultReportPageReqDTO reqDTO);
 
     void addToFaultInfo(FaultInfoDO faultInfo);
 
