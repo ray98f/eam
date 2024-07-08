94e0ca7ebf1f7e1b59d7e4da704a1e9cc700b5a9
fengruipeng <fengruipeng>
2024-07-08 09:39:17 +0800

    fix:修改故障详情返回

--- src/main/java/com/wzmtr/eam/impl/fault/TrackQueryServiceImpl.java
+++ src/main/java/com/wzmtr/eam/impl/fault/TrackQueryServiceImpl.java
@@ -93,6 +93,13 @@ public class TrackQueryServiceImpl implements TrackQueryService {
         if (StringUtils.isNotEmpty(faultFlows)) {
             faultDetail.setFlows(faultFlows);
         }
+        if (StringUtils.isNotEmpty(faultDetail.getPosition2Code())) {
+            Dictionaries dict = dictService.queryOneByItemCodeAndCodesetCode(
+                    CommonConstants.AT_STATION_POS2, faultDetail.getPosition2Code());
+            if (StringUtils.isNotNull(dict)) {
+                faultDetail.setPosition2Name(dict.getItemCname());
+            }
+        }
         // 待阅（实际为代办）更新为已办
         overTodoService.overTodo(faultOrder.getFaultOrderRecId(), "已查看", CommonConstants.TWO_STRING);
         return faultDetail;
