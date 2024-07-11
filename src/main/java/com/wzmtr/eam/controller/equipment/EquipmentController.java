5124eb1862842108ad7ec5dd953887a90b244ae2
fengruipeng <fengruipeng>
2024-07-11 09:23:35 +0800

    fix:修改导出

--- src/main/java/com/wzmtr/eam/controller/equipment/EquipmentController.java
+++ src/main/java/com/wzmtr/eam/controller/equipment/EquipmentController.java
@@ -12,10 +12,7 @@ import com.wzmtr.eam.entity.BaseIdsEntity;
 import com.wzmtr.eam.entity.PageReqDTO;
 import com.wzmtr.eam.entity.response.DataResponse;
 import com.wzmtr.eam.entity.response.PageResponse;
-import com.wzmtr.eam.enums.ErrorCode;
-import com.wzmtr.eam.exception.CommonException;
 import com.wzmtr.eam.service.equipment.EquipmentService;
-import com.wzmtr.eam.utils.StringUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -36,7 +33,6 @@ import javax.validation.Valid;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * 设备管理-设备台账
@@ -202,9 +198,9 @@ public class EquipmentController {
     @PostMapping("/export")
     @ApiOperation(value = "导出设备台账")
     public void exportEquipment(@RequestBody BaseIdsEntity baseIdsEntity, HttpServletResponse response) throws IOException {
-        if (Objects.isNull(baseIdsEntity) || StringUtils.isEmpty(baseIdsEntity.getIds())) {
-            throw new CommonException(ErrorCode.NORMAL_ERROR, "请先勾选后导出");
-        }
+//        if (Objects.isNull(baseIdsEntity) || StringUtils.isEmpty(baseIdsEntity.getIds())) {
+//            throw new CommonException(ErrorCode.NORMAL_ERROR, "请先勾选后导出");
+//        }
         equipmentService.exportEquipment(baseIdsEntity.getIds(), response);
     }
 
